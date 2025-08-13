d49c80f500e8067ef687d6c2edd7ff610ef0a5c5
PARVA147 <dongaparva111@gmail.com>
2025-08-13 12:34:19 +0530

    Add files via upload

---
+++ convert m to  h.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float hour,minutes;
+printf("enter minutes:");
+scanf("%f",&minutes);
+hour=minutes/60;
+printf("\n hour= %f:",hour);
+getch();
+}
