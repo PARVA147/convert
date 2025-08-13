22ace2dfbc66f798e83949c21813ed6fa5f3455b
PARVA147 <dongaparva111@gmail.com>
2025-08-13 12:30:08 +0530

    Add files via upload

---
+++ div c.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{int num1,num2,num3;
+printf("enter num1:");
+scanf("%d",&num1);
+printf("enter num2:");
+scanf("%d",&num2);
+num3=num1/num2;
+printf("/n%d/%d=%d:",num1,num2,num3);
+getch();
+}
